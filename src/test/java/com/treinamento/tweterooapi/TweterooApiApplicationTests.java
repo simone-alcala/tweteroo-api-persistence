b7a3bba8cf53b88fff6102f04d779fd3a0c33ab2
simone.alcala <simone.alcala@gmail.com>
2023-01-18 16:12:17 -0300

    Chore: First commit

---
+++ src/test/java/com/treinamento/tweterooapi/TweterooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.tweterooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
