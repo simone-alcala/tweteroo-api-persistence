79a6611d5b94f83e4e9b6787b9429df224be6763
simone.alcala <simone.alcala@gmail.com>
2023-01-22 16:12:10 -0300

    Feat: Implement Auth controller

---
+++ src/main/java/com/tweteroo/api/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.tweteroo.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping
+public class AuthController {
+
+  @Autowired
+  private UserService service;
+
+  @PostMapping("/sign-in")
+  //@ResponseStatus(code = HttpStatus.CREATED)
+  public void create(@RequestBody @Valid UserDTO req) {
+    service.create(req);
+  }
+
+}
