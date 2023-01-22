642b3495b57b354e99a38f51ef74638f1fab53f6
simone.alcala <simone.alcala@gmail.com>
2023-01-22 16:15:16 -0300

    Feat: Implement Tweet controller

---
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -0,0 +1,45 @@
+package com.tweteroo.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.TweetDTO;
+import com.tweteroo.api.projection.TweetProjection;
+import com.tweteroo.api.service.TweetService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/tweets")
+public class TweetController {
+  
+  @Autowired
+  private TweetService service;
+
+  @PostMapping
+  public void create(@RequestBody @Valid TweetDTO req) {
+    service.create(req);
+  }
+
+  @GetMapping
+  public List<TweetProjection> findAll(@PageableDefault(page = 0, size = 5) Pageable page) {
+    return service.findAll(page);
+  }
+
+  @GetMapping("/{username}")
+  public List<TweetProjection> findByUsername(@PathVariable("username") String username) {
+    return service.findByUsername(username);
+  }
+  
+}
