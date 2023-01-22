04da7ab44a660d39e860f6a8dce243b4b20c48f8
simone.alcala <simone.alcala@gmail.com>
2023-01-22 16:09:58 -0300

    Chore: Database setup

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_users (username, avatar) VALUES ('morticia', 'https://64.media.tumblr.com/5c901164cf28859fd65c39310a425a24/80dfb64d6b0e3331-57/s400x600/c8f6c5c591f600a32252ed28e75bdb4276e132d1.jpg');
+INSERT INTO tb_users (username, avatar) VALUES ('wednesday', 'https://64.media.tumblr.com/2392d1f73c8ceb4442ee0cac2fb6ce7c/tumblr_o1209nsLp11t10zkyo7_250.png');
+
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('1', 'Darling, I always wear black.');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('1', 'Don’t torture yourself, Gomez. That’s my job.');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('1', 'All the important numbers: Police, Fire Department, Morgue.');
+
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('2', 'We don’t hug.');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('2', 'I’ll clean my room in exchange for your immortal soul.');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('2', 'I am not in a bad mood, this is just me.');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('2', 'How I wish something would lighten up this already tedious day.');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('2', 'Nice knife! Can I play autopsy with it?');
+INSERT INTO tb_tweets (user_id, tweet) VALUES ('2', 'I believe the word is psychotic.');
