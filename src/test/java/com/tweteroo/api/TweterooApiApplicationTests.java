97c7cdfaf2110ffa4099c677cc64966ec154f439
simone.alcala <simone.alcala@gmail.com>
2023-01-22 16:15:34 -0300

    Chore: test

---
+++ src/test/java/com/tweteroo/api/TweterooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweteroo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
