ed019ee4f6cd080ab758f58116a2902673295330
simone.alcala <simone.alcala@gmail.com>
2023-01-22 21:36:40 -0300

    Feat: Implement EntityNotFound exception

---
+++ src/main/java/com/tweteroo/api/service/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tweteroo.api.service.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+  
+}
